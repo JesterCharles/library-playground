bf85f2134b2667904820b8ff72fa1362ca6e3fb4
Charles Jester <charles.jester@revature.com>
2024-10-11 14:53:48 -0400

    Included the update, delete and insert implementation for our MemberDao

--- Week-2/LibraryApp/src/main/java/com/revature/LibraryApp/daos/MemberDao.java
+++ Week-2/LibraryApp/src/main/java/com/revature/LibraryApp/daos/MemberDao.java
@@ -4,6 +4,7 @@ import com.revature.LibraryApp.models.Member;
 import com.revature.LibraryApp.util.ConnectionFactory;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,13 +16,13 @@ public class MemberDao {
     public List<Member> findAll() {
         List<Member> members = new ArrayList<>();
 
-        try(Connection connection = ConnectionFactory.getConnectionFactory().getConnection()){
+        try (Connection connection = ConnectionFactory.getConnectionFactory().getConnection()) {
             Statement statement = connection.createStatement();
             String sql = "SELECT * FROM members";
 
             ResultSet resultSet = statement.executeQuery(sql);
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 Member member = new Member();
                 member.setMemberId(resultSet.getInt("member_id"));
                 member.setName(resultSet.getString("name"));
@@ -39,15 +40,86 @@ public class MemberDao {
     }
 
     public Member create(Member member) {
-        return null;
+        try (Connection connection = ConnectionFactory.getConnectionFactory().getConnection()) {
+            // Create a new record in our database
+
+            // NOTE : using Statement interface DOES NOT sanitize the input to prevent SQL injection
+//            Statement statement = connection.createStatement();
+//            String sql = "INSERT INTO members (name, email, password) VALUES ('" + member.getName() + "', '"
+//                    + member.getEmail() + "', '" + member.getPassword() + "')";
+//
+//            statement.executeUpdate(sql);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO members (name, password, email) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            // Values in our SQL statement are still parameterized
+            // for SQL the parameters are 1-indexed
+            preparedStatement.setString(1, member.getName());
+            preparedStatement.setString(2, member.getPassword());
+            preparedStatement.setString(3, member.getEmail());
+            // All of the values have been set in their parameterized positions
+
+            int checkInsert = preparedStatement.executeUpdate();
+            if (checkInsert <= 0) {
+                throw new RuntimeException("Inserting member failed");
+            }
+            preparedStatement.getGeneratedKeys().next(); // move from column header to the first row
+            member.setMemberId(preparedStatement.getGeneratedKeys().getInt(1)); // get value for the first column (CANNOT use table column name)
+
+            return member;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
     }
 
     public Member update(Member member) {
+        try (Connection connection = ConnectionFactory.getConnectionFactory().getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE members SET name = ?, password = ?, email = ? where member_id = ?"
+            );
+            // Values in our SQL statement are still parameterized
+            // for SQL the parameters are 1-indexed
+            preparedStatement.setString(1, member.getName());
+            preparedStatement.setString(2, member.getPassword());
+            preparedStatement.setString(3, member.getEmail());
+            preparedStatement.setInt(4, member.getMemberId());
+            // All of the values have been set in their parameterized positions
 
-        return null;
-    }
+            int checkInsert = preparedStatement.executeUpdate();
+            if (checkInsert <= 0) {
+                throw new RuntimeException("Updating member failed");
+            }
+
+            return member;
+        } catch(Exception e){
+            e.printStackTrace();
+
+            return null;
+        }
+}
+
 
     public boolean delete(int memberId) {
-        return false;
+        try (Connection connection = ConnectionFactory.getConnectionFactory().getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "DELETE FROM members WHERE member_id = ?"
+            );
+            preparedStatement.setInt(1, memberId);
+
+            int checkInsert = preparedStatement.executeUpdate();
+            if (checkInsert <= 0) {
+                throw new RuntimeException("Deleting member failed");
+            }
+
+            return true;
+        } catch(Exception e){
+            e.printStackTrace();
+
+            return false;
+        }
     }
 }
--- Week-2/LibraryApp/src/main/resources/library-sql-statements.sql
+++ Week-2/LibraryApp/src/main/resources/library-sql-statements.sql
@@ -30,6 +30,10 @@ CREATE TABLE books (
 INSERT INTO members (name, email, password) VALUES
     ('John Doe', 'johndoe@example.com', 'password123'),
     ('Jane Smith', 'janesmith@example.com', 'password456');
+   
+INSERT INTO members (name, email, password) VALUES
+    ('Henry Ni', 'henryni@example.com', 'password123!'),
+    ('Thomas Kay', 'thomaskay@example.com', 'password456!');
     
 INSERT INTO authors (name, biography, birth_date) VALUES
     ('Stephen King', 'A horror novelist', '1947-09-21'),
